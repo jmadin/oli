636a6d0694ccbcde67cba3c9305b1b47f8952e53
Joshua Madin <jmadin@hawaii.edu>
2021-02-22 21:18:22 -1000

    Figures included.

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# A geometric basis for surface habitat complexity and biodiversity
+# Surface complexity for Oli
 
-The `analysis.R` code will reproduce the analyses and figures for the research posted currently at [bioRxiv](https://www.biorxiv.org/content/10.1101/2020.02.03.929521v1). The paper has been provisionally accepted at *Nature Ecology & Evolution*.
+You need to pop the data file you sent me called "3D Points - WGR-P05 - Layer 1 and 3 Combined.txt" in the `data/oli` folder.
 
-The `example.R` code contains a generic example of how the code might be used to calculate the surface descriptors for from any DEM. There are essentially three steps:
+Then you should be able to just run the `analysis.R` code. There are essentially these steps:
 
 1. Select the size of the patch for your calculations (`L`)
 2. Select the scales across which fractal dimension will be calculated (`scl`), the smallest scale automatically is the resolution (`L0`)
-3. Load a DEM file as `data` (geotif)
+3. Load a DEM file as `data` (geotif). Here we load your `xyz` file and create a raster. This is a hack, but just to give you a feel for the process.
 4. Pick the bottom left point of the patch (`x0` and `y0`)
 5. Run the `height_variation()` function, which is essentially a wrapper function (see `R/functions.R`). This function only requires the variables mentioned so far (`L`, `scl`, `L0`, `data`, `x0` and `y0`), so make sure there're assigned.  The output of the `height_variation()` function is the DEM height range the prescribed scales. For the largest scale `H` (or 2x2m) there is only one value for the entire patch, and the next scale down (1x1m) there are four values, and so on. Best to assign the output to a variable and save it somewhere (see the Example), because the smallest scales are very time-consuming.
 6. Calculate the surface descriptor metrics using the `rdh()` function. This function only requires the output from `height_variation()`. `rdh()` sends back a list with several metrics:
@@ -20,7 +20,7 @@ R | Surface rugosity calculated using `surfaceArea` function in R
 R_theory | Surface rugosity calculated from theory ()
 H | The height range (or height range at L)
 
-### ToDo
+### Example results
 
-- Apply to uses with 3D mesh
-- Look at issues with D when large drop-offs in DEM (can cause D to go below 2)
+![](figs/fig1.png)
+![](figs/fig2.png)
--- analysis.R
+++ analysis.R
@@ -3,7 +3,7 @@ source("R/functions.R")
 
 # Scope (extent), scales of variation, and resolution (grain)
 L <- 150 # Scope, 2 by 2 m reef patches
-scl <- L / c(1, 2, 4, 8) #, 16, 32, 64, 128) # Scales, aim for 2 orders of magnitude
+scl <- L / c(1, 2, 4, 8, 16) #, 16, 32, 64, 128) # Scales, aim for 2 orders of magnitude
 L0 <- min(scl) # Grain, resolution of processing ~ 6 cm
 
 # Example surface (an 8x8m section of Horseshoe from Lizard Island)
@@ -11,10 +11,10 @@ output <- "oli" # For housekeeping
 
 # Load example geotif
 muss <- read.table("data/oli/3D Points - WGR-P05 - Layer 1 and 3 Combined.txt", header=FALSE, as.is=TRUE)
-r <- raster(ncols=500, nrows=500) # kind of coarse for example
+r <- raster(ncols=400, nrows=400) # kind of coarse for example
 data <- rasterize(muss[c("V1", "V2")], r, muss[c("V3")], fun=max, ask=TRUE)
 
-pdf("figs/fig.pdf", width = 8, height = 8)
+png("figs/fig1.png", width = 8, height = 8, units = 'in', res = 300)
 
 image(data, asp=1)
 
@@ -24,7 +24,6 @@ x0 <- -L/2 #data@extent[1]
 y0 <- -L/2 #data@extent[3]
 rect(x0, y0, x0+L, y0+L, border="white", lty=2)
 
-dev.off()
 
 # Calulate height variation at different scales (scl) within patch, and save output (because a time-consuming step)
 example <- height_variation(write=TRUE, return=TRUE)
@@ -33,4 +32,41 @@ example <- height_variation(write=TRUE, return=TRUE)
 example <- read.csv(paste0("output/", output, "/var_", names(data), "_0001.csv"), as.is=TRUE)
 
 # Calculate rugosit, fractal dimension and height range (rdh function)
-rdh(example)
+out1 <- rdh(example)
+
+text(120, 50, paste0("D = ", round(out1$D, 2)))
+text(120, 30, paste0("R = ", round(out1$R, 2)))
+text(120, 10, paste0("H = ", round(out1$H, 2)))
+
+dev.off()
+
+### BELOW REPEATS FOR MINIMA
+
+data <- rasterize(muss[c("V1", "V2")], r, muss[c("V3")], fun=min, ask=TRUE)
+
+png("figs/fig2.png", width = 8, height = 8, units = 'in', res = 300)
+
+image(data, asp=1)
+
+rep <- 1
+# Choose patch in which to calculate RDH (rugosity, fractal D and height range).
+x0 <- -L/2 #data@extent[1]
+y0 <- -L/2 #data@extent[3]
+rect(x0, y0, x0+L, y0+L, border="white", lty=2)
+
+# Calulate height variation at different scales (scl) within patch, and save output (because a time-consuming step)
+example <- height_variation(write=TRUE, return=TRUE)
+
+# Load the file if starting here:
+example <- read.csv(paste0("output/", output, "/var_", names(data), "_0001.csv"), as.is=TRUE)
+
+# Calculate rugosit, fractal dimension and height range (rdh function)
+out2 <- rdh(example)
+
+text(120, 50, paste0("D = ", round(out2$D, 2)))
+text(120, 30, paste0("R = ", round(out2$R, 2)))
+text(120, 10, paste0("H = ", round(out2$H, 2)))
+
+dev.off()
+
+
