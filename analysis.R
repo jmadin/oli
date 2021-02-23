282592ef4bd9e0999565d9f56bdbb3ddee21cd4b
Joshua Madin <jmadin@hawaii.edu>
2021-02-22 21:19:00 -1000

    Forgot to save.

--- analysis.R
+++ analysis.R
@@ -3,7 +3,7 @@ source("R/functions.R")
 
 # Scope (extent), scales of variation, and resolution (grain)
 L <- 150 # Scope, 2 by 2 m reef patches
-scl <- L / c(1, 2, 4, 8, 16) #, 16, 32, 64, 128) # Scales, aim for 2 orders of magnitude
+scl <- L / c(1, 2, 4, 8, 16, 32) #, 16, 32, 64, 128) # Scales, aim for 2 orders of magnitude
 L0 <- min(scl) # Grain, resolution of processing ~ 6 cm
 
 # Example surface (an 8x8m section of Horseshoe from Lizard Island)
@@ -11,6 +11,7 @@ output <- "oli" # For housekeeping
 
 # Load example geotif
 muss <- read.table("data/oli/3D Points - WGR-P05 - Layer 1 and 3 Combined.txt", header=FALSE, as.is=TRUE)
+
 r <- raster(ncols=400, nrows=400) # kind of coarse for example
 data <- rasterize(muss[c("V1", "V2")], r, muss[c("V3")], fun=max, ask=TRUE)
 
